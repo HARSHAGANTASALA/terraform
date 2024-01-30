fe9a897ff6ecf87db6f06a345ca06955fb626ea8
HARSHAGANTASALA <107869095+HARSHAGANTASALA@users.noreply.github.com>
2024-01-30 10:31:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,25 +64,27 @@ Name = "app-server-2"
 
 resource "aws_security_group" "five" {
 name = 'elb-sg"
+
 ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 ingress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 egress {
-from_port = 0
-to_port = 0
-protocol = "-1"
-cidr_blocks = ["0.0.0.0/0"]
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_s3_bucket" "six" {
